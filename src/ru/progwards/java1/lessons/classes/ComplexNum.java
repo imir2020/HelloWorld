ced15820898b510b93207b9a51d1991bd3951150
Vladimir <imir2020@yandex.ru>
2020-08-24 18:08:53 +0300

    The third program

--- src/ru/progwards/java1/lessons/classes/ComplexNum.java
+++ src/ru/progwards/java1/lessons/classes/ComplexNum.java
@@ -13,34 +13,38 @@ public class ComplexNum {
     }
 
     public String toString() {
-        return a + " + " + b + "i";
+        return a + "+" + b + "i";
     }
 
     public ComplexNum add(ComplexNum num) {
-        num.z = (num.a + num.c) + (num.b + num.d);
+        num.a = a + num.a;
+        num.b = b + num.b;
         return num;
     }
 
     public ComplexNum sub(ComplexNum num) {
-        num.z = (num.a - num.c) + (num.b - num.d);
+        num.a = a - num.a;
+        num.b = b - num.b;
         return num;
     }
 
     public ComplexNum mul(ComplexNum num) {
-        num.z = (num.a *num.c - num.b * num.d) + (num.b * num.c + num.a * num.d);
-        return  num;
+        num.a = (a * num.a - b * num.b);
+        num.b = (b * num.a + a * num.b);
+        return num;
     }
 
-    public ComplexNum div(ComplexNum num){
-        num.z = (num.a *num.c + num.b * num.d)/(num.c * num.c + num.d * num.d)
-                + ((num.b *num.c - num.a *num.d)/(num.c * num.c + num.d * num.d));
+    public ComplexNum div(ComplexNum num) {
+        num.a = (a * num.a + b * num.b) / (num.a * num.a + num.b * num.b);
+        num.b = ((b * num.a + a * num.b) / (num.a * num.a + num.b * num.b));
         return num;
     }
 
     public static void main(String[] args) {
-        ComplexNum second = new ComplexNum(24, 94);
+        ComplexNum second = new ComplexNum(1, 1);
+        ComplexNum second2 = new ComplexNum(3, 2);
         System.out.println(second.toString());
-        System.out.println(second.add(new ComplexNum(53, 75)));
-        System.out.println();
+        System.out.println(second.sub(second2));
+        System.out.println(second.mul(second2));
     }
 }
