61d3df79d9cb0076f5fbcc51dcc3547dbf22f6c5
Vladimir <imir2020@yandex.ru>
2020-09-14 15:42:44 +0300

    The fourth program

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -3,50 +3,65 @@ package ru.progwards.java1.lessons.arrays;
 import java.util.Arrays;
 
 public class DIntArray {
-    private int index = 0;
-    private int[] someInt = new int[0];
-    private int[] newSomeInt;
+
+    private int ind = 0;
+    private int[] first = new int[0];
+    //private int[] second;
+
 
     public DIntArray() {
     }
 
     public void add(int num) {
-        someInt = Arrays.copyOf(someInt, someInt.length + 1);
-        someInt[index] = num;
-        index++;
+        first = Arrays.copyOf(first, first.length + 1);
+        first[first.length -1] = num;
+        ind++;
+        //test
+        for (int i = 0; i < first.length; i++) {
+            System.out.print(first[i]);
+        }
+        System.out.println();
     }
 
     public void atInsert(int pos, int num) {
-        newSomeInt = new int[someInt.length + 1];
-        for (int i = 0; i < someInt.length; i++) {
-            if (i < pos) {
-                newSomeInt[i] = someInt[i];
-            } else {
-                newSomeInt[i + 1] = someInt[i];
+        int[] temp  = Arrays.copyOf(first, first.length + 1 );
+        for (int i = 0; i < first.length; i++) {
+            temp[i] = first[i];
+        }
+        for (int i = temp.length - 1 ; i >= pos; i--) {
+            if(i > pos){
+                temp[i] = temp[i -1];
+            }
+            if(i == pos) {
+                temp[pos] = num;
             }
         }
-        newSomeInt[pos] = num;
+        first = Arrays.copyOf(temp, temp.length );
+        //test
+        for (int i = 0; i < temp.length; i++) {
+            System.out.print(first[i] + " ");
+        }
     }
 
     public void atDelete(int pos) {
-        int[] newSomeInt = new int[someInt.length - 1];
-        for (int i = 0; i < someInt.length; i++) {
+        int[] temp  = Arrays.copyOf(first, first.length - 1 );
+        for (int i = 0; i < temp.length; i++) {
             if (i < pos) {
-                newSomeInt[i] = someInt[i];
+                temp[i] = first[i];
             }
-            if (someInt[i] == someInt[pos]) {
-                break;
+            if(i >= pos){
+                temp[i] = first[i +1];
             }
         }
-        for (int j = pos; j < newSomeInt.length; j++) {
-            newSomeInt[j] = someInt[j + 1];
+        first = Arrays.copyOf(temp, temp.length );
+        //test
+        for (int i = 0; i < temp.length; i++) {
+            System.out.print(first[i] + " ");
         }
     }
-
-    public int at(int pos) {
-        return newSomeInt[pos];
+    public int at(int pos){
+        return first[pos];
     }
-
     public static void main(String[] args) {
         DIntArray one = new DIntArray();
         one.add(-3);
@@ -54,9 +69,12 @@ public class DIntArray {
         one.add(-7);
         one.add(9);
         System.out.println();
-        one.atDelete(2);
+        //System.out.println(one.ind);
         one.atInsert(3, 6745);
-        //one.at(3);
-        System.out.println(one.at(3));
+        System.out.println();
+         one.atDelete(2);
+        System.out.println();
+        one.at(3);
+        System.out.println(one.at(2));
     }
 }
