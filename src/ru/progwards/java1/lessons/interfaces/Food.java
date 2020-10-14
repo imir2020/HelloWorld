399f01c2b814796512d8e5045b2d0b2166dd235e
Vladimir <imir2020@yandex.ru>
2020-10-14 21:14:06 +0300

    The fourth program

--- src/ru/progwards/java1/lessons/interfaces/Food.java
+++ src/ru/progwards/java1/lessons/interfaces/Food.java
@@ -26,20 +26,7 @@ public class Food implements CompareWeight {
         }
         return result;
     }
-    public static void sort(int[] a) {
-        for (int i = 0; i < a.length; i++) {
-            for (int j = i + 1; j < a.length; j++) {
-                if (a[i] > a[j]) {
-                    int temp;
-                    temp = a[i];
-                    a[i] = a[j];
-                    a[j] = temp;
-                }
-            }
-        }
-    }
-
-
+   
     public static void sort(CompareWeight[] a) {
        Food[] mas = (Food[]) new CompareWeight[10];
         for (int i = 0; i < a.length; i++) {
