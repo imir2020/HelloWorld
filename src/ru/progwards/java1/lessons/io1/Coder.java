7bb4b9b9cdb3c2a4e9a95ed11c2a74af633f06d6
Vladimir <imir2020@yandex.ru>
2020-11-18 20:41:12 +0300

    The tenth program

--- src/ru/progwards/java1/lessons/io1/Coder.java
+++ src/ru/progwards/java1/lessons/io1/Coder.java
@@ -1,9 +1,29 @@
 package ru.progwards.java1.lessons.io1;
 
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Coder {
 
     public static void codeFile(String inFileName, String outFileName, char[] code, String logName) {
+        try {
+            FileInputStream readInFile = new FileInputStream(inFileName);
+            FileOutputStream writeInFile = new FileOutputStream(outFileName);
+            try {
+                while(readInFile.available() >=0){
+                }
+            } finally {
+                readInFile.close();
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+            
+        }
+
+
 
     }
 
