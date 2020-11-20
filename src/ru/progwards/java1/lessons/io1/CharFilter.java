a1b80ce6b9599b8fb7c9c2725aaba3632822a018
Vladimir <imir2020@yandex.ru>
2020-11-20 22:04:32 +0300

    The tenth program

--- src/ru/progwards/java1/lessons/io1/CharFilter.java
+++ src/ru/progwards/java1/lessons/io1/CharFilter.java
@@ -1,9 +1,16 @@
 package ru.progwards.java1.lessons.io1;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class CharFilter {
 
     public static void filterFile(String inFileName, String outFileName, String filter) {
-
+        try {
+            FileReader reader = new FileReader(inFileName);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
--- src/ru/progwards/java1/lessons/io1/Coder.java
+++ src/ru/progwards/java1/lessons/io1/Coder.java
@@ -4,33 +4,57 @@ package ru.progwards.java1.lessons.io1;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Coder {
 
     public static void codeFile(String inFileName, String outFileName, char[] code, String logName) {
+
+        FileReader readInFile = null;
         try {
-            FileReader readInFile = new FileReader(inFileName);
+            readInFile = new FileReader(inFileName);
             FileWriter writeInFile = new FileWriter(outFileName);
-            try {
-                int symbol;
-                while ((symbol = readInFile.read()) != -1) {
-                    System.out.println(symbol + " inPut");
-                    System.out.println((char) symbol + " output");//test
-                    writeInFile.write((char) symbol);
-                }
-            } finally {
-                readInFile.close();
-                writeInFile.close();
-            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        try {
+            int symbol;
+            ArrayList<Character> list = new ArrayList<>();
+            //char massSymbol;
+            while ((symbol = readInFile.read()) != -1) {
+                list.add((char) symbol);
+            }
+            // что-то похожее на шифрование.
+            for (int i = 0; i < list.size(); i++) {
+                list.add(i, (char) (list.get(i) + code[i]));
+            }
+            //Block for test
+            for (char s : list) {//
+                System.out.println(s);
+            }
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+
+        }
+
+
+        //System.out.println(symbol + " inPut");//test
+        // System.out.println((char) symbol + " output");//test
+        //writeInFile.write((char) symbol);
+        //Попробовать считать данные из файла с помощью readOfBytes()  в байтовый  массив
+        // затем зашифровать  байты в цикле(посмотреть в сети, как это можно делать),
+        //  записать в конечный файл
+        // и обработать исключение на длину массива - OutOfBoundException
+
     }
 
     public static void main(String[] args) {
         Coder coder = new Coder();
         char[] code = {3, 8};
-        String logName = " Something";
+        String logName = "Something";
         coder.codeFile("src/ru/progwards/java1/lessons/io1/inFileName",
                 "src/ru/progwards/java1/lessons/io1/outFileName", code, logName);
     }
