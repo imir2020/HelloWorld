4e83abb0dc1b4d85adf39da13683457bf10bee8b
Vladimir <imir2020@yandex.ru>
2020-11-22 19:12:07 +0300

    The tenth program

--- src/ru/progwards/java1/lessons/io1/CharFilter.java
+++ src/ru/progwards/java1/lessons/io1/CharFilter.java
@@ -2,19 +2,45 @@ package ru.progwards.java1.lessons.io1;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class CharFilter {
-
-    public static void filterFile(String inFileName, String outFileName, String filter) {
-        try {
-            FileReader reader = new FileReader(inFileName);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+    private static String deleteFilterChars(String string, String filter) {
+        StringBuilder builder = new StringBuilder();
+        int lOfString = string.length();
+        for (int i = 0; i < lOfString; i++) {
+            if (filter.indexOf(string.charAt(i)) < 0) {
+                builder.append(string.charAt(i));
+            }
         }
+        return builder.toString();
+    }
+
+    public static void filterFile(String inFileName, String outFileName, String filter) throws IOException {
+        FileReader reader = new FileReader(inFileName);
+        Scanner sc = new Scanner(reader);
+        FileWriter writer = new FileWriter(outFileName);
+        while (sc.hasNextLine()) {
+            String str = sc.nextLine();
+            str = deleteFilterChars(str, filter);
+            writer.write(str);
+        }
+        reader.close();
+        writer.close();
+        sc.close();
     }
 
     public static void main(String[] args) {
-        CharFilter charFilter = new CharFilter();
-        // charFilter.
+        try {
+            filterFile("src/ru/progwards/java1/lessons/io1/inFileName",
+                    "src/ru/progwards/java1/lessons/io1/outFileName",
+                    " -,.()");
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/ru/progwards/java1/lessons/io1/Coder.java
+++ src/ru/progwards/java1/lessons/io1/Coder.java
@@ -6,56 +6,66 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/*
+Проверить класс с помощью робота, посмотреть на выдаваемые ошибки, что возможно - устранить, остальное
+выяснить на консультации.
+
+Читать файлы можно и ПОСТРОЧНО, но перекодировать - только посимвольно.
+ */
+
 public class Coder {
 
     public static void codeFile(String inFileName, String outFileName, char[] code, String logName) {
 
-        FileReader readInFile = null;
+        FileReader readInFile;
+        FileWriter writeInFile;
         try {
             readInFile = new FileReader(inFileName);
-            FileWriter writeInFile = new FileWriter(outFileName);
-        } catch (IOException e) {
+            writeInFile = new FileWriter(outFileName);
+
+            try {
+                int symbol;
+                ArrayList<Character> buffer = new ArrayList<>();
+
+                while ((symbol = readInFile.read()) != -1) {
+                    buffer.add((char) symbol);
+                }
+
+                char key = code[0];
+
+                // что-то похожее на шифрование.
+                for (int i = 0; i < buffer.size(); i++) {
+                    buffer.set(i, (char) (buffer.get(i) + key));
+                    writeInFile.write(buffer.get(i));
+                }
+
+                //Block for test
+                for (char s : buffer) {
+                    System.out.println(s);
+                }
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                // какие конкретно ошибки должно "отлавливать" это исключение?
+                writeInFile = new FileWriter("logName");
+                writeInFile.write(ex.getMessage() + " Mistake");
+
+            } finally {
+                readInFile.close();
+                writeInFile.close();
+            }
+        } catch (IOException etx) {
+            etx.printStackTrace();
+        } catch (IndexOutOfBoundsException e) {
             e.printStackTrace();
         }
-
-        try {
-            int symbol;
-            ArrayList<Character> list = new ArrayList<>();
-            //char massSymbol;
-            while ((symbol = readInFile.read()) != -1) {
-                list.add((char) symbol);
-            }
-            // что-то похожее на шифрование.
-            for (int i = 0; i < list.size(); i++) {
-                list.add(i, (char) (list.get(i) + code[i]));
-            }
-            //Block for test
-            for (char s : list) {//
-                System.out.println(s);
-            }
-
-
-        } catch (IOException ex) {
-            ex.printStackTrace();
-
-        }
-
-
-        //System.out.println(symbol + " inPut");//test
-        // System.out.println((char) symbol + " output");//test
-        //writeInFile.write((char) symbol);
-        //Попробовать считать данные из файла с помощью readOfBytes()  в байтовый  массив
-        // затем зашифровать  байты в цикле(посмотреть в сети, как это можно делать),
-        //  записать в конечный файл
-        // и обработать исключение на длину массива - OutOfBoundException
-
     }
 
     public static void main(String[] args) {
         Coder coder = new Coder();
-        char[] code = {3, 8};
-        String logName = "Something";
+        char[] code = {3};
         coder.codeFile("src/ru/progwards/java1/lessons/io1/inFileName",
-                "src/ru/progwards/java1/lessons/io1/outFileName", code, logName);
+                "src/ru/progwards/java1/lessons/io1/outFileName", code,
+                "src/ru/progwards/java1/lessons/io1/logName");
     }
 }
