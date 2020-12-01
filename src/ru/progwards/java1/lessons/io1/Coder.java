c35d98f0c71d6ddf3e1ee859b317c39456a1918d
Vladimir <imir2020@yandex.ru>
2020-12-01 19:48:57 +0300

    The tenth program

--- src/ru/progwards/java1/lessons/io1/Coder.java
+++ src/ru/progwards/java1/lessons/io1/Coder.java
@@ -4,14 +4,6 @@ package ru.progwards.java1.lessons.io1;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-
-/*
-Проверить класс с помощью робота, посмотреть на выдаваемые ошибки, что возможно - устранить, остальное
-выяснить на консультации.
-
-Читать файлы можно и ПОСТРОЧНО, но перекодировать - только посимвольно.
- */
 
 public class Coder {
 
@@ -20,43 +12,33 @@ public class Coder {
         FileWriter writeInFile = new FileWriter(outFileName, false);
         try {
             int symbol;
-            ArrayList<Character> buffer = new ArrayList<>();
-
-            while ((symbol = readInFile.read()) != -1) {
-                buffer.add((char) symbol);
+            try {
+                while ((symbol = readInFile.read()) != -1) {
+                   // System.out.println(symbol);//test
+                    writeInFile.write(code[symbol]); //hint&tips
+                }
+            } finally {
+                writeInFile.close();
             }
-//            while (readInFile.ready()) {
-//                symbol
-//                buffer.add((char) symbol);
-//            }
-            char key = code[0];
-
-            // что-то похожее на шифрование.
-            for (int i = 0; i < buffer.size(); i++) {
-                buffer.set(i, (char) (buffer.get(i) + key));
-                writeInFile.write(buffer.get(i));
-            }
-
-            //Block for test
-            for (char s : buffer) {
-                System.out.println(s);
-            }
-
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.getMessage();
         } catch (IOException ex) {
-            ex.printStackTrace();
-            // какие конкретно ошибки должно "отлавливать" это исключение?
             writeInFile = new FileWriter("logName");
-            writeInFile.write(ex.getMessage() + " Mistake");
+            writeInFile.write(ex.getMessage());
         } finally {
-            readInFile.close();
-            writeInFile.close();
+            try {
+                readInFile.close();
+            } catch (IOException ignored) {
+            }
+            try {
+                writeInFile.close();
+            } catch (IOException ignored) {
+            }
         }
-
     }
 
     public static void main(String[] args) {
-
-        char[] code = {3};
+        char[] code = new char[8213];
         try {
             codeFile("src/ru/progwards/java1/lessons/io1/inFileName",
                     "src/ru/progwards/java1/lessons/io1/outFileName", code,
