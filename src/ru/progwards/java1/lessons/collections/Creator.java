323784406958a0a1c9c18063fd598da3989f9662
Vladimir <imir2020@yandex.ru>
2020-12-14 19:57:52 +0300

    The twelfth program

--- src/ru/progwards/java1/lessons/collections/Creator.java
+++ src/ru/progwards/java1/lessons/collections/Creator.java
@@ -14,38 +14,38 @@ public class Creator {
         return evenNumbersList;
     }
 
+//    public static Collection<Integer> fillOdd(int n) {
+//        int firstOddNumbers = n * 2 - 1;
+//        Collection<Integer> oddNumbersList = new ArrayList<>();
+//        for (int i = n; i > 0; i-- ){
+//            firstOddNumbers -= 2;
+//            oddNumbersList.add(firstOddNumbers);
+//        }
+//
+//        return oddNumbersList;
+//    }
     public static Collection<Integer> fillOdd(int n) {
-        int oddNumbers = n * 2 - 1;
         Collection<Integer> oddNumbersList = new ArrayList<>();
-        for (int i = n; i > 0; i-- ){
-            oddNumbers -= 2;
-            oddNumbersList.add(oddNumbers);
+        for (int i = n; i > 0 ; i-- ){
+           int firstOddNumbers = i * 2 - 1;
+            oddNumbersList.add(firstOddNumbers);
         }
-
         return oddNumbersList;
     }
 
     public static Collection<Integer> fill3(int n) {
         Collection<Integer> tripleNumbersList = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            switch (i % 3) {
-                case 0:
-                    tripleNumbersList.add(i);
-                    break;
-                case 1:
-                    tripleNumbersList.add(i * i);
-                    break;
-                case 2:
-                    tripleNumbersList.add(i * i * i);
-                    break;
-            }
+            tripleNumbersList.add(i);
+            tripleNumbersList.add(i*i);
+            tripleNumbersList.add(i*i*i);
         }
         return tripleNumbersList;
     }
 
     public static void main(String[] args) {
-        System.out.println(fillEven(90));
-        System.out.println(fillOdd(90));
-        System.out.println( fill3(90));
+       // System.out.println(fillEven(90));
+       // System.out.println(fillOdd(19));
+        System.out.println( fill3(3));
     }
 }
