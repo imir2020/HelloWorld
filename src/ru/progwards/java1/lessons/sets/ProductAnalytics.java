9faaefea56e1f21d212e090d9b3f38f0ea163c08
Vladimir <imir2020@yandex.ru>
2020-12-23 15:51:08 +0300

    The thirteenth program

--- src/ru/progwards/java1/lessons/sets/ProductAnalytics.java
+++ src/ru/progwards/java1/lessons/sets/ProductAnalytics.java
@@ -12,8 +12,8 @@ public class ProductAnalytics {
     }
 
     public Set<Product> existInAll() {
-        if(products == null || products.size() == 0) return  new HashSet<>();
-        if(shops == null || shops.size() == 0) return  new HashSet<>();
+        if (products == null || products.size() == 0) return new HashSet<>();
+        if (shops == null || shops.size() == 0) return new HashSet<>();
         Set<Product> productList = new HashSet(products);
         Iterator it = shops.iterator();
         while (it.hasNext()) {
@@ -23,16 +23,16 @@ public class ProductAnalytics {
     }
 
     public Set<Product> existAtListInOne() {
-        if(products == null || products.size() == 0) return  new HashSet<>();
+        if (products == null || products.size() == 0) return new HashSet<>();
         Iterator it = shops.iterator();
-        Set<Product> productList = new HashSet(((Shop)it.next()).getProducts());
+        Set<Product> productList = new HashSet(((Shop) it.next()).getProducts());
 
         while (it.hasNext()) {
-            productList.addAll(new HashSet<>(((Shop)it.next()).getProducts()));
+            productList.addAll(new HashSet<>(((Shop) it.next()).getProducts()));
         }
         productList.retainAll(products);
 
-        return null;
+        return productList;
     }
 
     public Set<Product> notExistInShops() {
@@ -90,8 +90,8 @@ public class ProductAnalytics {
         Shop s1 = new Shop(new ArrayList<Product>(Arrays.asList(p2, p7, p8, p10)));
         Shop s2 = new Shop(new ArrayList<Product>(Arrays.asList(p1, p4, p6, p10)));
         Shop s3 = new Shop(new ArrayList<Product>(Arrays.asList(p1, p4, p5, p10)));
-        List<Shop> ss = new ArrayList<Shop>(Arrays.asList(s1, s2, s3));
-        List<Product> pa = new ArrayList<Product>(Arrays.asList(p5, p7, p8, p10));
+        List<Shop> ss = new ArrayList(Arrays.asList(s1, s2, s3));
+        List<Product> pa = new ArrayList(Arrays.asList(p5, p7, p8, p10));
         ProductAnalytics productAnalytics = new ProductAnalytics(ss, pa);
         System.out.println(productAnalytics.existOnlyInOne());
     }
