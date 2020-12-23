db57a9ae926942ddebf5bc7eeb0c800c79f96fe3
Vladimir <imir2020@yandex.ru>
2020-12-23 15:33:57 +0300

    The thirteenth program

--- src/ru/progwards/java1/lessons/sets/LettersInFile.java
+++ src/ru/progwards/java1/lessons/sets/LettersInFile.java
@@ -1,13 +1,85 @@
 package ru.progwards.java1.lessons.sets;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 public class LettersInFile {
-    public static String process(String fileName) throws IOException {
 
-        return  null;
+    public static String process1(String fileName) throws IOException {
+        Set<Character> set = new HashSet<Character>();
+        FileReader fileReader = new FileReader(fileName);
+        Scanner scanner = new Scanner(fileReader);
+        while (scanner.hasNextLine()) {
+            String str = scanner.nextLine();
+            str.toCharArray();
+            char[] charArray = str.toCharArray();
+            for (char aChar : charArray) {
+                if (Character.isLetter(aChar)) set.add(aChar);
+            }
+        }
+        scanner.close();
+        fileReader.close();
+
+        List<Character> list = new ArrayList<>(set);
+        Collections.sort(list);
+        StringBuilder sb = new StringBuilder(100);
+        for (Character aChar : list) {
+            sb.append(aChar);
+        }
+        return sb.toString();
     }
-    public static void main(String[] args) {
 
+    public static String process2(String fileName) throws IOException {
+        String set = "";
+        FileReader fileReader = new FileReader(fileName);
+        Scanner scanner = new Scanner(fileReader);
+        while (scanner.hasNextLine()) {
+            String str = scanner.nextLine();
+            str.toCharArray();
+            char[] charArray = str.toCharArray();
+            for (char aChar : charArray) {
+                if (Character.isLetter(aChar)) {
+                    if (set.indexOf(aChar) == -1) set += aChar;
+                }
+            }
+        }
+        scanner.close();
+        fileReader.close();
+
+        char[] charArray = set.toCharArray();
+        List<Character> list = new ArrayList<>();
+        for (char aChar : charArray) {
+            list.add(aChar);
+        }
+        Collections.sort(list);
+        StringBuilder sb = new StringBuilder(100);
+        for (Character aChar : list) {
+            sb.append(aChar);
+        }
+        return sb.toString();
+    }
+
+    public static String process(String fileName) throws IOException {
+        return process1(fileName);
+    }
+
+    public static void processFilesFromFolder(File folder) throws IOException {
+        File[] folderEntries = folder.listFiles();
+        for (File entry : folderEntries) {
+            if (entry.isDirectory()) {
+                processFilesFromFolder(entry);
+                continue;
+            } else {
+                if (entry.getName().indexOf(".java") > 0) {
+                    process(entry.getPath());
+                }
+            }
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+      
     }
 }
--- src/ru/progwards/java1/lessons/sets/ProductAnalytics.java
+++ src/ru/progwards/java1/lessons/sets/ProductAnalytics.java
@@ -1,9 +1,6 @@
 package ru.progwards.java1.lessons.sets;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class ProductAnalytics {
     private List<Shop> shops;
@@ -15,33 +12,88 @@ public class ProductAnalytics {
     }
 
     public Set<Product> existInAll() {
+        if(products == null || products.size() == 0) return  new HashSet<>();
+        if(shops == null || shops.size() == 0) return  new HashSet<>();
         Set<Product> productList = new HashSet(products);
         Iterator it = shops.iterator();
         while (it.hasNext()) {
-            productList.retainAll(new HashSet<Product>(((Shop) it.next()).getProducts()));
+            productList.retainAll(new HashSet<>(((Shop) it.next()).getProducts()));//Нужно понять до конца, как работает эта строка.
         }
         return productList;
     }
 
     public Set<Product> existAtListInOne() {
+        if(products == null || products.size() == 0) return  new HashSet<>();
+        Iterator it = shops.iterator();
+        Set<Product> productList = new HashSet(((Shop)it.next()).getProducts());
 
+        while (it.hasNext()) {
+            productList.addAll(new HashSet<>(((Shop)it.next()).getProducts()));
+        }
+        productList.retainAll(products);
 
         return null;
     }
 
     public Set<Product> notExistInShops() {
+        if (products == null || products.size() == 0) return new HashSet<>();
 
+        Set<Product> productList = new HashSet(products);
+        if (shops == null || shops.size() == 0) return productList;
 
-        return null;
+        Iterator it = shops.iterator();
+        while (it.hasNext()) {
+            productList.removeAll(new HashSet<Product>(((Shop) it.next()).getProducts()));
+        }
+        return productList;
+
     }
 
     public Set<Product> existOnlyInOne() {
+        Set<Product> productList = new HashSet<Product>();
+        if (products == null || products.size() == 0) return productList;
+        if (shops == null || shops.size() == 0) return productList;
+        for (int i = 0; i < shops.size(); i++) {
+            Set<Product> productsBefore = new HashSet<Product>();
+            Set<Product> productsOne = new HashSet<Product>();
+            Set<Product> productsAfter;
+            Iterator it = shops.iterator();
+            for (int k = 0; k < shops.size(); k++) {
+                productsAfter = new HashSet<Product>(((Shop) it.next()).getProducts());
+                productsAfter.retainAll(products);
+                if (k < i) {
+                    productsBefore.addAll(productsAfter);
+                } else if (k == i) {
+                    productsOne = productsAfter;
+                    productsOne.removeAll(productsBefore);
+                } else {
+                    productsOne.removeAll(productsAfter);
+                }
+            }
+            productList.addAll(productsOne);
+        }
 
-        return null;
+        return productList;
     }
 
     public static void main(String[] args) {
-
+        Product p1 = new Product("art-1");
+        Product p2 = new Product("art-2");
+        Product p3 = new Product("art-3");
+        Product p4 = new Product("art-4");
+        Product p5 = new Product("art-5");
+        Product p6 = new Product("art-6");
+        Product p7 = new Product("art-7");
+        Product p8 = new Product("art-8");
+        Product p9 = new Product("art-9");
+        Product p10 = new Product("art-10");
+        Shop s1 = new Shop(new ArrayList<Product>(Arrays.asList(p2, p7, p8, p10)));
+        Shop s2 = new Shop(new ArrayList<Product>(Arrays.asList(p1, p4, p6, p10)));
+        Shop s3 = new Shop(new ArrayList<Product>(Arrays.asList(p1, p4, p5, p10)));
+        List<Shop> ss = new ArrayList<Shop>(Arrays.asList(s1, s2, s3));
+        List<Product> pa = new ArrayList<Product>(Arrays.asList(p5, p7, p8, p10));
+        ProductAnalytics productAnalytics = new ProductAnalytics(ss, pa);
+        System.out.println(productAnalytics.existOnlyInOne());
     }
 
 }
